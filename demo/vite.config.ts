dc46066e905d3940d1d85888a8d18542b1d44dd4
Andrey Pavlenko <31999255+andrey-pavlenko@users.noreply.github.com>
2022-05-06 20:25:26 +0300

    Prepare to package

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -3,9 +3,14 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	clearScreen: false,
-	plugins: [svelte()],
-	optimizeDeps: {
-		include: ['highlight.js', 'highlight.js/lib/core']
-	}
+  clearScreen: false,
+  plugins: [svelte()],
+  optimizeDeps: {
+    include: ['highlight.js', 'highlight.js/lib/core']
+  },
+  build: {
+    outDir: '../docs',
+    emptyOutDir: true,
+    target: 'es2015'
+  }
 });
